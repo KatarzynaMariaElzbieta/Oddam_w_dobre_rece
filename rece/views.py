4124852a84cadaa66a1dea88f999b7dcc51d9962
KatarzynaMariaElzbieta <kasia.gajc@wp.pl>
2020-06-18 22:42:45 +0200

    dynamiczne statystyki

--- rece/views.py
+++ rece/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Donation, Institution
 # Create your views here.
 
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html', {})
+        dotations = Donation.objects.all()
+        sum = 0
+        for d in dotations:
+            sum += d.quantity
+        institutions = Institution.objects.all().count()
+        ctx = {
+            'sum_dotations': sum,
+            'sum_institutions': institutions
+        }
+        return render(request, 'index.html', ctx)
 
 
 class AddDonation(View):
